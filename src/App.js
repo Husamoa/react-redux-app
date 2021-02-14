7ac6d41e063a69466f721467297505dc0812f1b3
Pawel Bialek <pawel.bialek@e-file.pl>
2021-02-14 16:40:48 +0100

    TodoList styled list material ui

--- src/App.js
+++ src/App.js
@@ -3,18 +3,36 @@ import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 import VisibilityFilters from "./components/VisibilityFilters";
 import {Container, Grid} from "@material-ui/core";
+import {makeStyles} from '@material-ui/core/styles';
 import "./styles/App.scss";
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        textAlign: "center"
+    },
+}));
 export default function App() {
+    const classes = useStyles();
+
+
     return (
-        <Container className="todo-app">
-            <Grid alignContent="center">
-            <h1>Todo List</h1>
-            <AddTodo />
-            <TodoList />
-            <VisibilityFilters />
+        <div className={classes.root}>
+            <Grid container className="todo-app">
+                <Grid item xs={12}>
+                    <h1>Todo List</h1>
+                </Grid>
+                <Grid item xs={12}>
+                    <AddTodo/>
+                </Grid>
+                <Grid item xs={12}>
+                    <TodoList/>
+                </Grid>
+                <Grid item xs={12}>
+                    <VisibilityFilters/>
+                </Grid>
             </Grid>
-        </Container>
+        </div>
     );
 }
 
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,19 +2,23 @@ import React from "react";
 import { connect } from "react-redux";
 import cx from "classnames";
 import { toggleTodo } from "../redux/actions";
+import {Container, Divider, List, ListItemText} from "@material-ui/core";
 
 const Todo = ({ todo, toggleTodo }) => (
-    <li className="todo-item" onClick={() => toggleTodo(todo.id)}>
-        {todo && todo.completed ? "👌" : "👋"}{" "}
-        <span
+    <Container>
+    <List className="todo-item" onClick={() => toggleTodo(todo.id)}>
+        <ListItemText
             className={cx(
                 "todo-item__text",
                 todo && todo.completed && "todo-item__text--completed"
             )}
         >
-      {todo.content}
-    </span>
-    </li>
+            {todo && todo.completed ? "👌" : "👋"}{" "}
+            {todo.content}
+    </ListItemText>
+        <Divider />
+    </List>
+    </Container>
 );
 
 // export default Todo;
